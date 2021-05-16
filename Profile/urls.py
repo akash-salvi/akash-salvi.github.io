6267012ee5cb58b80ec4b1e4330da4f11d596355
Akash-Salvi <akashnov100@gmail.com>
2021-05-16 10:26:41 +0530

    Changes Made

--- Profile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-       # Home page Url
-       path('',views.home,name='home'),
-      
-]
--- README.md
+++
@@ -1,1 +0,0 @@
-# akash-salvi.github.io
